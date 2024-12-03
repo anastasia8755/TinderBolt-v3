2eb919b91739b00ac2c1f6413a1d9ae9d49d015b
anastasia8755 <anastasia.sydor331@gmail.com>
2024-12-03 14:03:40 +0100

    1st hw

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -1,19 +1,13 @@
 package com.javarush.telegram;
 
-import com.javarush.telegram.ChatGPTService;
-import com.javarush.telegram.DialogMode;
-import com.javarush.telegram.MultiSessionTelegramBot;
-import com.javarush.telegram.UserInfo;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.objects.*;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.util.ArrayList;
-
 public class TinderBoltApp extends MultiSessionTelegramBot {
-    public static final String TELEGRAM_BOT_NAME = "bot-name"; //TODO: додай ім'я бота в лапках
-    public static final String TELEGRAM_BOT_TOKEN = "bot-token"; //TODO: додай токен бота в лапках
+    public static final String TELEGRAM_BOT_NAME = "little_tg_tinder_helper_bot";
+    public static final String TELEGRAM_BOT_TOKEN = "7520928912:AAFhpTwA5rYzB5TYJ3v3C7fBtZfMp1zglro";
     public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: додай токен ChatGPT у лапках
 
     public TinderBoltApp() {
@@ -22,8 +16,25 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update update) {
-        //TODO: основний функціонал бота будемо писати тут
 
+        String message = getMessageText();
+
+        if (message.equals("/start")) {
+            sendTextMessage("*Hello*!");
+            sendPhotoMessage("main");
+            return;
+
+        } else if (message.equals("/stop")) {
+            sendTextMessage("*Bye*!");
+            sendPhotoMessage("bye");
+            return;
+        }
+
+        sendTextMessage("_" + message + "_");
+
+        sendTextButtonsMessage("Button message",
+                "START", "start",
+                "STOP", "stop");
     }
 
     public static void main(String[] args) throws TelegramApiException {
